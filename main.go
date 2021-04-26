c4d8283cde054704b549977b8ab88d973bb261c3
Raffaele Cassia <me@raffaelecassia.it>
2021-04-26 15:24:52 +0200

    Add alpn, cert+key, CAroot flags

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -26,6 +30,10 @@ func main() {
 	clientID := flag.String("clientid", "", "client id")
 	username := flag.String("username", "", "username")
 	password := flag.String("password", "", "password")
+	alpnproto := flag.String("alpn", "", "ALPN protocol")
+	certfile := flag.String("cert", "", "client certificate PEM")
+	keyfile := flag.String("key", "", "client private key PEM")
+	cafile := flag.String("cafile", "", "CA root certificate PEM")
 	flag.Parse()
 	topics := flag.Args()
 
@@ -34,11 +42,43 @@ func main() {
 	if *clientID != "" {
 		bb.options.SetClientID(*clientID)
 	}
+
+	// basic username/password
 	if *username != "" {
 		bb.options.SetUsername(*username)
 	}
 	if *password != "" {
 		bb.options.SetPassword(*password)
+	}
+
+	// certs
+	tlsConfig := tls.Config{}
+	setTlsConfig := false
+	if *certfile != "" && *keyfile != "" {
+		// https://pkg.go.dev/crypto/tls#LoadX509KeyPair
+		cert, err := tls.LoadX509KeyPair(*certfile, *keyfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+		setTlsConfig = true
+	}
+	if *cafile != "" {
+		certPool := x509.NewCertPool()
+		pemData, err := ioutil.ReadFile(*cafile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		certPool.AppendCertsFromPEM(pemData)
+		tlsConfig.RootCAs = certPool
+		setTlsConfig = true
+	}
+	if *alpnproto != "" {
+		tlsConfig.NextProtos = []string{*alpnproto}
+		setTlsConfig = true
+	}
+	if setTlsConfig {
+		bb.options.SetTLSConfig(&tlsConfig)
 	}
 
 	// connecting
